67f65e30d6d6cbd28853054d2f9d868d20fac9ce
armacoty <astronom_35@mail.ru>
2021-02-23 11:28:18 +0300

    Добавил PyMySql

---
+++ config.py
@@ -0,0 +1,6 @@
+import typing
+
+db_login : str = "root"
+db_password : str = "SecretPassword"
+
+# TODO: From env
--- hello.py
+++ hello.py
@@ -5,7 +5,9 @@ app : Flask = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world() -> str:
-    return "Hello world!"
+    return """
+    <pre>/index</pre> page
+    """
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
