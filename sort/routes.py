be54d5d00ef74772d89c59fc9b11d3436fb72072
Alexey-Sennikov <nesnaiker@yandex.ru>
2021-03-04 15:10:37 +0300

    Запрос на и добавление новой мусорки и  изменение ее веса

--- sort/routes.py
+++ sort/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request
 
 from sort import app, db
-from sort.models import Message
+from sort.models import Message, TrashCan
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -22,3 +22,28 @@ def add_message():
     db.session.commit()
 
     return redirect(url_for('main'))
+
+
+@app.route('/change_state', methods=['POST'])
+def change_state():
+    if not request.json or not 'id' in request.json:
+        abort(400)
+
+    i = request.json['id']
+    w = request.json['weight']
+
+    try:
+        TrashCan.query.filter_by(id = i).update({TrashCan.weight: w})
+        db.session.commit()        
+    except:
+        db.session.add(TrashCan(i, w))
+        db.session.flush()
+        db.session.commit()
+            
+    return '200'
+
+
+@app.route('/trash_cans', methods=['GET'])
+def show_trash_cans():
+    trash_cans = TrashCan.query.all()
+    return render_template('trash_cans.html', trash_cans = trash_cans)
