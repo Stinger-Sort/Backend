94d608ae1e6c0245c424f27ad639b778e920fb9e
Alexey-Sennikov <nesnaiker@yandex.ru>
2021-03-06 12:54:49 +0300

    поправил обработчик запроса на создание и обновление мусорки

---
+++ app.py
@@ -0,0 +1,4 @@
+from sort import app
+
+if __name__ == "__main__":
+    app.run(host='armacoty.tk', port=5000, debug=True)
--- main.py
+++
@@ -1,4 +0,0 @@
-from sort import app
-
-if __name__ == "__main__":
-    app.run()
