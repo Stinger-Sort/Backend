2f17b366138b2166ee5758c67ce41851ceb349a8
Pavivin <pavivin@yandex.ru>
2021-03-24 14:57:12 +0300

    refactor

--- sort/__init__.py
+++ sort/__init__.py
@@ -1,9 +1,9 @@
+from datetime import timedelta
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
-
-import datetime
 
 from config import login, password, database, mail_password
 
@@ -11,13 +11,14 @@ app = Flask(__name__)
 app.secret_key = 'some secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://{login}:{password}@localhost/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.permanent_session_lifetime = datetime.timedelta(days=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'sort.app.yar@gmail.com'  
-app.config['MAIL_DEFAULT_SENDER'] = 'sort.app.yar@gmail.com'  
+app.config['MAIL_USERNAME'] = 'sort.app.yar@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'sort.app.yar@gmail.com'
 app.config['MAIL_PASSWORD'] = mail_password
 
 mail = Mail(app)
