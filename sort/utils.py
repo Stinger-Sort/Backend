43ceee1b7a4aaff81ea81c4936960c88f926ae69
Ивин Павел <pavivin@yandex.ru>
2021-04-02 10:26:08 +0300

    upload_photo, change_state, required_validator (#24)

--- sort/utils.py
+++ sort/utils.py
@@ -1,17 +1,27 @@
+from flask import abort
 from flask_mail import Message
-from sort import app, mail
-
-LOGIN_SUBJECT = 'Код для авторизации в приложении Sort'
-SENDER = 'sort.app.yar@gmail.com'
+from sort import mail
 
 
-def trash_counter(trash):
-    """Сумма баллов пользователя"""
+def trash_counter(trash: dict):
+    """Общий вес мусора"""
     return sum(trash.values())
 
 
-def send_email(recipients, html_body, text_body=' ', subject=LOGIN_SUBJECT, sender=SENDER):
+def send_email(recipients, html_body):
+    """Отправка кода для регистрации пользователя"""
+
+    subject = 'Код для авторизации в приложении Sort'
+    sender = 'sort.app.yar@gmail.com'
+    text_body = 'Sort'
+
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+
+
+def required_fields(fields: tuple, record: dict):
+    """Проверка запроса на необходимые поля"""
+    if fields not in record:
+        abort(400)
