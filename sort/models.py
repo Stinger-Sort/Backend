64b932cb4d00507d31bc4f080128d2b5417e0eed
Pavivin <pavivin@yandex.ru>
2021-04-21 02:15:59 +0300

    thread_email

--- sort/models.py
+++ sort/models.py
@@ -38,10 +38,10 @@ class User(UserBase):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(128))
-    first_name = db.Column(db.String(30))
-    last_name = db.Column(db.String(30))
-    city = db.Column(db.String(30))
-    phone_number = db.Column(db.String(15))
+    # first_name = db.Column(db.String(30))
+    # last_name = db.Column(db.String(30))
+    # city = db.Column(db.String(30))
+    # phone_number = db.Column(db.String(15))
 
     @hybrid_property
     def level(self):
